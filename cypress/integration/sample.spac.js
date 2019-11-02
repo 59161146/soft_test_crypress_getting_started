71770a3b54313a9a429a1ae0e2dbc077f10ba50e
Chirat Liabthawi <59161146@go.buu.ac.th>
2019-11-02 09:15:20 +0700

    first commit

---
+++ cypress/integration/sample.spac.js
@@ -0,0 +1,12 @@
+describe("Cypress Getting Started", () => {
+  it("Dose not do much thing", () => {
+    cy.visit("https://example.cypress.io");
+    cy.contains("type").click();
+    cy.url().should("include", "/commands/actions");
+    cy.get(".action-email")
+      .type("fake@email.com")
+      .should("have.value", "fake@email.com");
+    cy.get('.action-div').dblclick().should('not.be.visible')
+    cy.get('.action-input-hidden').should('be.visible')
+  });
+});
